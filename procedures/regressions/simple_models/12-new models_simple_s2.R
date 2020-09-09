2e1f2c2ed33afeb8f4ce7720e31d1b641e707199
christian <ccdelgado@u-bordeaux.fr>
2020-09-09 12:15:37 +0000

    202009091415

--- procedures/regressions/simple_models/12-new models_simple_s2.R
+++ procedures/regressions/simple_models/12-new models_simple_s2.R
@@ -1,8 +1,15 @@
 ################################################################################
 # New models for the S2
 ################################################################################
+database_ctt<-data.table::as.data.table(read.csv("../input/reg/s2c.csv"))
+database_pboc<-data.table::as.data.table(read.csv("../input/reg/s2p.csv"))
 
-formula<-"linked ~ ethnic + soc_2 + soc_3 + soc_4 + av_cent + absdif_cent + geodis + insprox + techprox"  
+database_ctt[is.na(database_ctt)] <- 0
++ insprox "Institutional proximity",
+
+# Attention, I have disabled the clustered
+
+formula<-"linked ~ ethnic + soc_2 + soc_3 + soc_4 + av_cent + absdif_cent + geodis + techprox"  
 labels<-c("Ethnic similarity",
           "Social distance = 2",
           "Social distance = 3",
@@ -10,10 +17,10 @@ labels<-c("Ethnic similarity",
           "Average centrality",
           "Abs. diff. centrality",
           "Geographic distance", 
-          "Institutional proximity",
+          
           "Technological proximity")
 # LPM with clustered errors, only for the small sample
-lpmshort<-simmodels(sformula=formula, labels_cov=labels, includenas=TRUE, logit=FALSE, clustered=TRUE, fpath="../input/reg", onlyt = TRUE)
+lpmshort<-simmodels(sformula=formula, labels_cov=labels, includenas=TRUE, logit=FALSE, clustered=FALSE, fpath="../input/reg", onlyt = TRUE)
 
 formula<-"linked ~ ethnic + soc_2 + soc_3 + soc_4 + av_cent + absdif_cent + geodis + same_appl "  
 labels<-c("Ethnic similarity",
@@ -25,7 +32,7 @@ labels<-c("Ethnic similarity",
           "Geographic distance", 
           "Same applicant")
 # LPM with clustered errors, only for the Large sample
-lpmshort<-simmodels(sformula=formula, labels_cov=labels, includenas=TRUE, logit=FALSE, clustered=TRUE, fpath="../input/reg", onlyt = TRUE)
+lpmshort<-simmodels(sformula=formula, labels_cov=labels, includenas=TRUE, logit=FALSE, clustered=FALSE, fpath="../input/reg", onlyt = TRUE)
 
 formula<-"linked ~ ethnic + soc_2 + soc_3 + soc_4 + av_cent + absdif_cent + geodis + techprox + same_appl "  
 labels<-c("Ethnic similarity",
